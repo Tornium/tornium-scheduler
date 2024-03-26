6fb30ad9ba8012453bc88981a879ec368b9d7caf
tiksan <webmaster@deek.sh>
2024-03-26 13:47:42 -0700

    Add basic sockets handling
    Signed-off-by: tiksan <webmaster@deek.sh>

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <boost/asio.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/local/datagram_protocol.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/system/detail/error_code.hpp>
+#include <iostream>
+
+#if not defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
+# error Local sockets not available on this platform.
+#endif
+
+using namespace boost;
+boost::asio::io_context io_context;
+boost::asio::steady_timer timer(io_context, boost::asio::chrono::seconds(5));
+
+class datagram_server {
+public:
+    datagram_server(boost::asio::io_context& io_context) : socket_(io_context, boost::asio::local::datagram_protocol::endpoint("/tmp/scheduler.sock")) {
+        do_receive();
+    }
+    
+    void do_receive() {
+        socket_.async_receive_from(
+            boost::asio::buffer(data_, 3), sender_endpoint_,
+            [this](boost::system::error_code error, std::size_t bytes_received) {
+                if (!error and bytes_received > 0) {
+                    std::cout << data_ << std::endl;
+                    do_send(bytes_received);
+                } else {
+                    do_receive();
+                }
+        });
+    }
+
+    void do_send(std::size_t length) {
+        socket_.async_send_to(
+            boost::asio::buffer("ACK", 3), sender_endpoint_,
+            [this](boost::system::error_code, std::size_t) {
+                do_receive();
+            });
+    }
+
+private:
+    boost::asio::local::datagram_protocol::socket socket_;
+    boost::asio::local::datagram_protocol::endpoint sender_endpoint_;
+    enum { max_length = 128 };
+    char data_[max_length];
+};
+
+void tick(const boost::system::error_code&) {
+    std::cout << "tick" << std::endl;
+    timer.expires_at(timer.expires_at() + boost::asio::chrono::seconds(5));
+    timer.async_wait(tick);
+}
+
+int main() {
+    ::unlink("/tmp/scheduler.sock");
+    datagram_server server(io_context);
+
+    timer.async_wait(tick);
+
+    io_context.run();
+
+    return 0;
+}
